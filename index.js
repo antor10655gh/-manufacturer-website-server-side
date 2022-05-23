88cb28755ca393994f505cd806bfcdb6bff51c58
antor10655gh <creativefreelancerantor@gmail.com>
2022-05-23 13:25:16 +0600

    add single product api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -23,11 +23,20 @@ async function run() {
     await client.connect();
     const productCollection = client.db("tooltrex").collection("products");
 
+    // create api for loaded all products
     app.get("/products", async (req, res) => {
       const query = {};
       const cursor = productCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
+    });
+
+    // create api for loaded single product
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
     });
   } finally {
   }
