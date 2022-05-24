db53f4edabf5858bf27d739a30f1892c5d6b4d75
antor10655gh <creativefreelancerantor@gmail.com>
2022-05-24 15:18:59 +0600

    create api to check user email role

--- index.js
+++ index.js
@@ -84,6 +84,14 @@ async function run() {
       res.send(result);
     });
 
+    // create api for check admin
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
     // create api for store all users on userCollection
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
